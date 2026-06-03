b0e39656b0545073fc762ebc5e8285ec3dfc28da
ilyamokka <ilyamokka@gmail.com>
2026-06-03 16:19:28 -0300

    Added mp4 support (somewhat)

--- picture-card.js
+++ picture-card.js
@@ -8,11 +8,12 @@ class PictureCard extends HTMLElement {
                 const color = this.getAttribute('bg');
                 var rl = this.getAttribute('rl');
                 var href = this.getAttribute('href');
+                const fallback = this.getAttribute('fallback');
+
                 if (!caption) caption = "";
                 if (!rl) rl = "nowhere";
                 setTimeout(() => {
                         if (href === "itself") {
-                        
                                 var images = "?"
                                 var index = 0
                                 var currentIndex = 0;
@@ -24,11 +25,37 @@ class PictureCard extends HTMLElement {
                                 href = `view.html${images}currentIndex=${currentIndex}`
                         
                         }
-                        if (href) this.innerHTML = `<div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;
-                height: fit-content;"><a href="${href}"><img src="${src}" style="width: ${width}; height: ${height};"></a><p class="image-text centered-at-the-bottom" style="font-size:${fontsize}; background-color:${color};">${caption}</p></div>`;
-                        else this.innerHTML = `<div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;
-                height: fit-content;"><img src="${src}" style="width: ${width}; height: ${height};"><p class="image-text centered-at-the-bottom" style="font-size:${fontsize}; background-color:${color};">${caption}</p></div>`;
+
+                        if (src.split('.')[1] === "mp4") {
+                                const path = src.split('/')
+                                const filename = path[path.length - 1].split('.')[0]
+
+                                if (href) this.innerHTML = `
+                                        <style>#${filename} {
+                                                width: ${width};
+                                                display:block;
+                                        }</style>
+
+                                        <a href="${href}" class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;height: fit-content;">
+                                                <video id="${filename}" loop autoplay muted><source src="${src}" type="video/mp4"/>
+                                                        <source src="${src}" type="video/mp4">
+                                                        <picture-card src="${src.split('.')[0]}.${fallback}" w="${width}" h="${height}" fs="${fontsize}" bg="${color}" cap="${caption}" rl="${rl}"></picture-card>
+                                                </video>
+                                        </a>`
+                                else this.innerHTML = `<div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;height: fit-content;"><video style="width: ${width};object-fit:cover;margin:auto;display:block;" loop autoplay muted><source src="${src}" type="video/mp4"/><picture-card src="${src.split('.')[0]}.${fallback}" w="${width}" h="${height}" fs="${fontsize}" bg="${color}" cap="${caption}" rl="${rl}"></picture-card></video></div>`
+                        } else {
+                                if (href) this.innerHTML = `<div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;
+                        height: fit-content;"><a href="${href}"><img src="${src}" style="width: ${width}; height: ${height};"></a><p class="image-text centered-at-the-bottom" style="font-size:${fontsize}; background-color:${color};">${caption}</p></div>`;
+                                else this.innerHTML = `<div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;
+                        height: fit-content;"><img src="${src}" style="width: ${width}; height: ${height};margin:auto;display:block;"><p class="image-text centered-at-the-bottom" style="font-size:${fontsize}; background-color:${color};">${caption}</p></div>`;
+                        }
                 }, 0)
+                
+        }
+
+        setAttribute(name, value) {
+                super.setAttribute(name, value)
+                if (name === "src") this.connectedCallback()
         }
 }
 customElements.define('picture-card', PictureCard);
