06debef73d3f4eb473e82c01401a2b7ec1edd5d7
ilyamokka <ilyamokka@gmail.com>
2026-07-10 15:12:30 -0300

    Another fix

--- picture-card.js
+++ picture-card.js
@@ -56,12 +56,12 @@ class PictureCard extends HTMLElement {
                                         display:block;
                                 }</style>
                                 
-                                <div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;height: fit-content;"><video id="${filename}" style="width: ${width};object-fit:contain;margin:auto;display:block;" loop autoplay muted playsinline><source src="${src}" type="video/mp4"/><picture-card src="${src.split('.')[0]}.${fallback}" w="${width}" h="${height}" bg="${color}" cap="${caption}${enlarge}" rl="${rl}"></picture-card></video></div><p class="image-text centered-at-the-bottom" background-color:${color};">${caption}${enlarge}</p>`
+                                <div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;height: fit-content;"><video id="${filename}" style="width: ${width};object-fit:contain;margin:auto;display:block;" loop autoplay muted playsinline><source src="${src}" type="video/mp4"/><picture-card src="${src.split('.')[0]}.${fallback}" w="${width}" h="${height}" bg="${color}" cap="${caption}${enlarge}" rl="${rl}"></picture-card></video><p class="image-text centered-at-the-bottom" background-color:${color};">${caption}${enlarge}</p></div>`
                         } else {
                                 if (href && href.length > 0) this.innerHTML = `<div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;
-                        height: fit-content;"><a href="${href}"><img src="${src}" style="width: ${width}; height: ${height};object-fit:contain;margin:auto;"></a></div><p class="image-text centered-at-the-bottom" background-color:${color};">${caption}${enlarge}</p>`;
+                        height: fit-content;"><a href="${href}"><img src="${src}" style="width: ${width}; height: ${height};object-fit:contain;margin:auto;"></a><p class="image-text centered-at-the-bottom" background-color:${color};">${caption}${enlarge}</p></div>`;
                                 else this.innerHTML = `<div class="image-card move-${rl}" style="display:block;margin:auto;width: fit-content;
-                        height: fit-content;"><img src="${src}" style="width: ${width}; height: ${height};margin:auto;display:block;object-fit:contain;"></div><p class="image-text centered-at-the-bottom" background-color:${color};">${caption}${enlarge}</p>`;
+                        height: fit-content;"><img src="${src}" style="width: ${width}; height: ${height};margin:auto;display:block;object-fit:contain;"><p class="image-text centered-at-the-bottom" background-color:${color};">${caption}${enlarge}</p></div>`;
                         }
                 
                         // if (!height) this.innerHTML += "<p style='color:red;'>BRUH</p>";
